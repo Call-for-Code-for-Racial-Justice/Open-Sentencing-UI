5b2d8e355f5bac83a1d7c2bdc871677d08727d45
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2021-02-20 12:06:32 -0500

    add new font styles and layout changes to login component

--- web/src/app/components/cases/cases.component.spec.ts
+++ web/src/app/components/cases/cases.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CaseComponent } from './cases.component';
 
@@ -6,7 +6,7 @@ describe('EmbraceComponent', () => {
   let component: CaseComponent;
   let fixture: ComponentFixture<CaseComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CaseComponent ]
     })
