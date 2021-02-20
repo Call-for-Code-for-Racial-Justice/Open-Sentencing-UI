50f0985127e52f5db851c1dd301e090ee576a0ba
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2021-02-20 12:06:32 -0500

    add new font styles and layout changes to login component
    Signed-off-by: Akilah Littlejohn <akilahlittlejohn@gmail.com>

--- web/src/app/components/embrace/embrace.component.spec.ts
+++ web/src/app/components/embrace/embrace.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EmbraceComponent } from './embrace.component';
 
@@ -6,7 +6,7 @@ describe('EmbraceComponent', () => {
   let component: EmbraceComponent;
   let fixture: ComponentFixture<EmbraceComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EmbraceComponent ]
     })
