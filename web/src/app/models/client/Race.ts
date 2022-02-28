15d8d880833db91cd90408443c894c0e5aa1ed29
edel <edel.altares@pm.me>
2022-02-27 18:20:57 -0700

    Update race options for client

--- web/src/app/models/client/Race.ts
+++ web/src/app/models/client/Race.ts
@@ -1,12 +1,14 @@
 export const Race = {
-  WHITE: "White",
-  BLACK: "Black",
-  HISPANIC: "Hispanic",
-  OTHER: "Other",
+  AMERICAN_INDIAN_OR_ALASKA_NATIVE: "American Indian or Alaska Native",
   ASIAN: "Asian",
-  AMERICAN_INDIAN: "American Indian",
-  BIRACIAL: "Biracial",
-  WHITE_HISPANIC: "White [Hispanic or Latino]",
-  WHITE_BLACK: "White/Black [Hispanic or Latino]",
-  UNKNOWN: "Unknown"
+  BLACK_OR_AFRICAN_AMERICAN: "Black or African American",
+  HISPANIC_OR_LATINO: "Hispanic or Latino",
+  NATIVE_HAWAIIAN_OR_OTHER_PACIFIC_ISLANDER: "Native Hawaiian or Other Pacific Islander",
+  WHITE: "White",
+  MULTI_RACIAL: "Multi-racial"
+}
+
+export function isValidRace(race: string): boolean {
+  return Object.keys(Race).indexOf(race) > - 1 ||
+          Object.values(Race).indexOf(race) > -1;
 }
